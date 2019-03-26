f70bce35655c74b3ff2e58f403f6e05108f9c706
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-26 17:22:06 +0100

    continuation

--- del_unn_files.py
+++ del_unn_files.py
@@ -8,7 +8,9 @@ import os
 import send2trash
 
 # TODO: Write function to print absolute path to files (100 MB and more file size).
-def search_print_big_files()
+def search_print_big_files(x):
+    if os.path.getsize(x) >= 104857600:
+        print('Direction greater than 100 MB: {}'.format(abs_path))
 
 # TODO: Walk selected directory with os.walk()
 # TODO: Change project status on github.
