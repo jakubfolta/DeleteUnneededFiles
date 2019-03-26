02bceb9f3d1f1394a8342263715dac2702d45e3f
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-26 17:09:37 +0100

    continuation

---
+++ del_unn_files.py
@@ -0,0 +1,11 @@
+#! python3
+
+# del_unn_files.py - A program that walks through a folder tree and searches for exceptionally large files or folders.
+# Print these files with their absolute path to the screen.
+
+# Import essential modules.
+import os
+
+# TODO: Write function to print absolute path to files (100 MB and more file size).
+# TODO: Walk selected directory with os.walk()
+# TODO: Change project status on github.
