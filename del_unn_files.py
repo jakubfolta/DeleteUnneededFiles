968a588beb5d6723861fd14b347f517bf33414e7
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-27 13:45:03 +0100

    continuation

--- del_unn_files.py
+++ del_unn_files.py
@@ -10,18 +10,15 @@ import send2trash
 # Set the directory to check.
 dir_to_check = r'C:\Users\ogi-8\Desktop\PythonProjects\DelUnneededFiles\Sample_folder_to_check'
 
-# Write function to print absolute path to files (100 MB and more file size).
-def search_print_big_files(x):
-    if os.path.getsize(x) >= 104857600:
-        print('Direction greater than 100 MB: {}'.format(x))
-
 # Walk selected directory with os.walk()
 for folder, subfolders, files in os.walk(dir_to_check):
-    abs_path = folder
+    folder_abs_path = folder
+    print(abs_path)
     search_print_big_files(abs_path)
     #print(abs_path)
     for file in files:
-        abs_path = os.path.join(folder, file)
+        file_abs_path = os.path.join(folder, file)
+        print(os.path.getsize(abs_path))
         search_print_big_files(abs_path)
         #print(abs_path)
 # TODO: Change project status on github.
