c19fa22c45820f680012c4ae88005936a8c59ff1
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-03 20:48:29 +0200

    solidifying knowledge

--- del_unneeded_files.py
+++ del_unneeded_files.py
@@ -14,7 +14,12 @@ file_size_to_del = 104857600
 # TODO: Walk through folder tree and search for big files.
 for folder, subfolders, files in os.walk(dir_to_check):
     total_size = 0
-    for file in files:
+    for x in os.listdir(folder):
+        if os.path.isdir(os.path.join(folder, x)):
+            print('yes')
+    # print(' '.join(subfolders))
+    # print(' '.join(subfolders) in folder)
+    '''for file in files:
         file_size = os.path.getsize(file)
         if file_size >= file_size_to_del:
             file_abspath = os.path.join(folder, file)
@@ -22,8 +27,10 @@ for folder, subfolders, files in os.walk(dir_to_check):
             #send2trash.send2trash(file_abspath)
             continue
         total_size = total_size + file_size
-        
-
+    if total_size >= file_size_to_del and folder != dir_to_check and subfolders not in folder:
+        print('Size of files greter than 100MB. Size: {}\nFolder:\n{}\nSend to trash.'.format(total_size, folder)))
+        #send2trash.send2trash(folder)
+'''
 
 
 # TODO:
