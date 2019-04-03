0465919060c7dac9b3b18c28496627d0516bc237
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-03 18:06:34 +0200

    solidifying knowledge

--- del_unneeded_files.py
+++ del_unneeded_files.py
@@ -3,10 +3,21 @@
 '''del_unneeded_files.py - A program that walks through a folder tree and searches for exceptionally large files or folders.
 Print these files with their absolute path to the screen.'''
 
-# TODO: Import essential modules.
+# Import essential modules.
+import os
+import send2trash
+
+# Set directory to check and file size to delete.
+dir_to_check = r'C:\Users\ogi-8\Desktop\PythonProjects\DelUnneededFiles\Sample_folder_to_check'
+file_size = 104857600
+
 # TODO: Walk through folder tree and search for big files.
-# TODO: 
-# TODO:
-# TODO:
-# TODO:
+for folder, subfolders, files in os.walk(dir_to_check):
+    total_size = 0
+    for file in files:
+        if os.path.getsize(file) >= file_size:
+            print()
+            #send2trash.send2trash
+
+
 # TODO:
