4bb8bef704dc605a99022d7e5b7c53e5e7f7fe78
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-03 18:15:41 +0200

    solidifying knowledge

--- del_unneeded_files.py
+++ del_unneeded_files.py
@@ -9,16 +9,20 @@ import send2trash
 
 # Set directory to check and file size to delete.
 dir_to_check = r'C:\Users\ogi-8\Desktop\PythonProjects\DelUnneededFiles\Sample_folder_to_check'
-file_size = 104857600
+file_size_to_del = 104857600
 
 # TODO: Walk through folder tree and search for big files.
 for folder, subfolders, files in os.walk(dir_to_check):
     total_size = 0
     for file in files:
-        if os.path.getsize(file) >= file_size:
+        file_size = os.path.getsize(file)
+        if file_size >= file_size_to_del:
             file_abspath = os.path.join(folder, file)
-            print('File size greater than 100MB. File:\n{}\nSend to trash.'.format(file_abspath))
+            print('File size greater than 100MB. Size: {}\nFile:\n{}\nSend to trash.'.format(file_size, file_abspath))
             #send2trash.send2trash(file_abspath)
+            continue
+        total_size = total_size + file_size
+        
 
 
 
