be8e3f51c93d293d14bc33d003e10a22ea7d780d
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-03 23:28:44 +0200

    solidifying knowledge

--- del_unneeded_files.py
+++ del_unneeded_files.py
@@ -14,24 +14,24 @@ file_size_to_del = 104857600
 # TODO: Walk through folder tree and search for big files.
 for folder, subfolders, files in os.walk(dir_to_check):
     total_size = 0
-    subfolder_exist = 0
-    print(''.join(subfolders) in folder)
+    #print(folder)
+    if subfolders:
+        print('y')
     # for x in os.listdir(folder):
     #     if os.path.isdir(os.path.join(folder, x)):
     #         subfolder_exists = 1
     #         break
-    # for file in files:
-    #     print(subfolder_exists)
-    #     file_abspath = os.path.join(folder, file)
-    #     file_size = os.path.getsize(file_abspath)
-    #     if file_size >= file_size_to_del:
-    #         print('File size greater than 100MB. Size: {}\nFile:\n{}\nSend to trash.'.format(file_size, file_abspath))
-    #         #send2trash.send2trash(file_abspath)
-    #         continue
-    #     total_size = total_size + file_size
-    # if total_size >= file_size_to_del and folder != dir_to_check: #and subfolder_exists == False:
-    #     print('Size of files greter than 100MB. Size: {}\nFolder:\n{}\nSend to trash.'.format(total_size, folder))
-    #     #send2trash.send2trash(folder)
+    for file in files:
+        file_abspath = os.path.join(folder, file)
+        file_size = os.path.getsize(file_abspath)
+        if file_size >= file_size_to_del:
+            print('File size greater than 100MB.\nSize of file: {}\n\nFile: "{}" in folder "{}" send to trash.'.format(file_size, file, os.path.basename(folder)))
+            #send2trash.send2trash(file_abspath)
+            continue
+        total_size = total_size + file_size
+    if total_size >= file_size_to_del and folder != dir_to_check and not subfolders:
+        print('Size of files greter than 100MB. \nSize of folder: {}\n\nFolder: "{}" send to trash.'.format(total_size, os.path.basename(folder)))
+        #send2trash.send2trash(folder)
 
 
 # TODO:
