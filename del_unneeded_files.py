98e0861c74e741c07c3bc3ff5eca917cf3e1a570
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-03 17:56:46 +0200

    solidifying knowledge

---
+++ del_unneeded_files.py
@@ -0,0 +1,12 @@
+#! python3
+
+'''del_unneeded_files.py - A program that walks through a folder tree and searches for exceptionally large files or folders.
+Print these files with their absolute path to the screen.'''
+
+# TODO: Import essential modules.
+# TODO: Walk through folder tree and search for big files.
+# TODO: 
+# TODO:
+# TODO:
+# TODO:
+# TODO:
