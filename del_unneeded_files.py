a41bd85d1256050daf6ad3aea2dd98f33a5cc477
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-03 21:14:27 +0200

    solidifying knowledge

--- del_unneeded_files.py
+++ del_unneeded_files.py
@@ -14,23 +14,23 @@ file_size_to_del = 104857600
 # TODO: Walk through folder tree and search for big files.
 for folder, subfolders, files in os.walk(dir_to_check):
     total_size = 0
+    subfolder_exist = 0
     for x in os.listdir(folder):
         if os.path.isdir(os.path.join(folder, x)):
-            print('yes')
-    # print(' '.join(subfolders))
-    # print(' '.join(subfolders) in folder)
-    '''for file in files:
-        file_size = os.path.getsize(file)
-        if file_size >= file_size_to_del:
-            file_abspath = os.path.join(folder, file)
-            print('File size greater than 100MB. Size: {}\nFile:\n{}\nSend to trash.'.format(file_size, file_abspath))
-            #send2trash.send2trash(file_abspath)
-            continue
-        total_size = total_size + file_size
-    if total_size >= file_size_to_del and folder != dir_to_check and subfolders not in folder:
-        print('Size of files greter than 100MB. Size: {}\nFolder:\n{}\nSend to trash.'.format(total_size, folder)))
-        #send2trash.send2trash(folder)
-'''
+            subfolder_exists = 1
+            break
+    for file in files:
+        print(subfolder_exists)
+    #     file_abspath = os.path.join(folder, file)
+    #     file_size = os.path.getsize(file_abspath)
+    #     if file_size >= file_size_to_del:
+    #         print('File size greater than 100MB. Size: {}\nFile:\n{}\nSend to trash.'.format(file_size, file_abspath))
+    #         #send2trash.send2trash(file_abspath)
+    #         continue
+    #     total_size = total_size + file_size
+    # if total_size >= file_size_to_del and folder != dir_to_check: #and subfolder_exists == False:
+    #     print('Size of files greter than 100MB. Size: {}\nFolder:\n{}\nSend to trash.'.format(total_size, folder))
+    #     #send2trash.send2trash(folder)
 
 
 # TODO:
