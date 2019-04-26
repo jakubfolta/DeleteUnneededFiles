496891cf85a4ec7e1663fcbea6c7707b925c6420
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-26 14:26:47 +0200

    solidifying knowledge

---
+++ delete_solidifying.py
@@ -0,0 +1,14 @@
+#! python3
+
+'''del_unneeded_files.py - A program that walks through a folder tree and searches for exceptionally large files or folders.
+Print these files with their absolute path to the screen.'''
+
+TODO:
+TODO: 
+TODO:
+TODO:
+TODO:
+TODO:
+TODO:
+TODO:
+TODO:
