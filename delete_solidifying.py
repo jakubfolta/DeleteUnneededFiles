4213a1b0ad647e021e9901e583274d1c0d4b1972
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-27 16:14:57 +0100

    solidifying knowledge

--- delete_solidifying.py
+++ delete_solidifying.py
@@ -3,11 +3,11 @@
 '''del_unneeded_files.py - A program that walks through a folder tree and searches for exceptionally large files or folders.
 Print these files with their absolute path to the screen.'''
 
-# TODO: Import essential modules.
+# Import essential modules.
+import os
+import send2trash
+
 # TODO: Set directory to check and other variables.
-# TODO: 
-# TODO:
-# TODO:
-# TODO:
+# TODO: Walk through given directory, check size of files and delete if needed.
 # TODO:
 # TODO:
