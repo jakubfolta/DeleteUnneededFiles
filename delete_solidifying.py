a618c7bb6e527f632961f52ff4bccfe2edf05d64
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-27 17:21:20 +0100

    solidifying knowledge

--- delete_solidifying.py
+++ delete_solidifying.py
@@ -13,12 +13,20 @@ logging.basicConfig(level = logging.DEBUG, format = '%(levelname)s, %(message)s'
 # Set directory to check and other variables.
 dir_to_check = r'C:\Users\ogi-8\Desktop\PythonProjects\DelUnneededFiles\Sample_folder_to_check'
 max_file_size = 104857600
+total_size = 0
 
 logging.info('Max file size is {}.'.format(max_file_size))
 # TODO: Walk through given directory, check size of files and delete if needed.
 for folder, subfolder, file in os.walk(dir_to_check):
-    logging.info(subfolder)
-    if subfolder:
-        print('yes')
+    file_dir = os.path.join(folder, file)
+    file_size = os.path.getsize(file)
+    logging.info('File size: {}'.format(file_size))
+
+    if file_size >= max_file_size:
+        send2trash.send2trash(file_dir)
+        continue
+    total_size += file_size
+    logging.info('Total size: {}'.format(total_size))
+
 # TODO:
 # TODO:
