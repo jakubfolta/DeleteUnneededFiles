4ccfdce64d4ff44bb2803fba006bfdb7015ac64d
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-28 09:47:16 +0100

    solidifying knowledge-refactoring

--- delete_solidifying.py
+++ delete_solidifying.py
@@ -21,14 +21,14 @@ logging.info('Max file size is {}.'.format(max_file_size))
 def delete_option():
     answer = ''
     while answer not in ['yes', 'no']:
-        print()
-        print('Do you want to delete files permanently? If "no", files will be send to trash where you can restore them.')
+        print('Do you want to delete " {} " permanently?'.format(file))
+        print('If "no", file will be send to trash where you can restore it.')
         answer = input('Type "yes" or "no": ')
     if answer == 'yes':
-        print('{} permanently deleted!'.format(file))
+        print('" {} " permanently deleted!\n'.format(file))
         #os.unlink(file_dir)
     elif answer == 'no':
-        print('{} sent to trash. You can still restore it.'.format(file))
+        print('" {} " sent to trash. You can still restore it.\n'.format(file))
         #send2trash.send2trash(file_dir)
 
 # Walk through given directory, check size of files and delete if needed.
@@ -39,21 +39,33 @@ for folder, subfolders, files in os.walk(dir_to_check):
     for file in files:
         file_size = os.path.getsize(os.path.join(folder, file))
         logging.info('File size: {}'.format(file_size))
+
         if file_size >= max_file_size:
             file_dir = os.path.join(folder, file)
+            print('" {} " greater than 100MB'.format(file))
             delete_option()
             continue
         total_size += file_size
         logging.info('Total size is {}'.format(total_size))
+
     if total_size >= max_file_size and not subfolders:
         logging.info('Total size of {} is {}.'.format(folder_basename, total_size))
-        print('Folder {} is greater than {}. Folder deleted.'.format(folder_basename, max_file_size))
-        #send2trash.send2trash(folder)
+        answer = ''
+        while answer not in ['yes', 'no']:
+            print('Folder " {} " is greater than {}.'.format(folder_basename, max_file_size))
+            print('Do you want to delete folder permanently? If "no", folder will be send to trash where you can restore it.')
+            answer = input('Type "yes" or "no": ')
+        if answer == 'yes':
+            print('" {} " permanently deleted!\n'.format(folder_basename))
+            #os.unlink(folder)
+        elif answer == 'no':
+            print('" {} " sent to trash. You can still restore it.\n'.format(folder_basename))
+            #send2trash.send2trash(folder)
+
     elif total_size >= max_file_size:
         print('Total size of files in {} is greater than {}. Files will be deleted.'.format(os.path.basename(folder), max_file_size))
         for file in files:
             file_dir = os.path.join(folder, file)
-            print('{} deleted.'.format(file))
-            #send2trash.send2trash(file_dir)
+            delete_option()
 else:
     print('Whole directory checked!')
